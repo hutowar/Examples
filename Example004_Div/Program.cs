fd9f11461eae9d2ce7bbd4050796d164e3cc2ec7
Gorzoth <hutowar1@gmail.com>
2023-07-31 22:58:37 +0300

    Example004 Div Project

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double NumberA = new Random().Next(1, 10); // 1 2 3 4 ... 10
+Console.WriteLine(NumberA);
+double NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
+double result = NumberA / NumberB;
+Console.WriteLine(NumberA + " / " + NumberB +" = "+ result);
