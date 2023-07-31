2a9b92c549c62cac1388b476793661683173530b
Gorzoth <hutowar1@gmail.com>
2023-07-31 23:11:17 +0300

    Example005 Condition If else project

---
+++ Example005_ConditionIfelse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your username: ");
+String username = Console.ReadLine();
+String Password = "david";
+if (username.ToLower() == "david"){
+    Console.Write("Access allowed. Welcome " + username + "!");
+}
+else {
+    Console.Write("Access denied.");
+}
