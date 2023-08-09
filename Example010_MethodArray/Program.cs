3d8dca4583dd8181619eb2f5b14bd8bf28534859
Gorzoth <hutowar1@gmail.com>
2023-08-09 14:45:54 +0300

    example010 method wwith array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿//              0  1  2  3  4  5  6  7  8
+int[] array = { 2, 7, 4, 4, 6, 2, 8, 4, 6 };
+int count = 0;
+int number = int.Parse(Console.ReadLine());
+while (number < array.Length)
+{
+    if (number == array[count])
+    {
+        Console.WriteLine(count);
+        break;
+    }
+    count++;
+}
