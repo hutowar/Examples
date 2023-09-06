2d9650117cc22828043edbcb42d7b8e7faff0a6f
Gorzoth <hutowar1@gmail.com>
2023-09-07 02:37:35 +0300

    Homework006 complete

---
+++ Homework006/task041/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void FillArray(int[] array, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue);
+    }
+}
+int ArrayNumberAboveZero (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+int min = InputNum("Введите минимальное число для массива: ");
+int max = InputNum("Введите максимальное число для массива: ");
+int arraySize = InputNum("Введите длинну массива: ");
+int[] myArray = new int[arraySize];
+FillArray(myArray, min, max);
+PrintArray(myArray);
+int m = ArrayNumberAboveZero(myArray);
+Console.WriteLine(m);
---
+++ Homework006/task043/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double InputNum (string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine()!);
+}
+void PointIntersection (double b1, double k1, double b2, double k2)
+{
+    double x = (b1-b2)/(k2-k1);
+    double y = (k2 * x + b2);
+    Console.WriteLine($"({x}; {y})");
+}
+double b1 = InputNum("Введите переменную b1: ");
+double k1 = InputNum("Введите переменную k1: ");
+double b2 = InputNum("Введите переменную b2: ");
+double k2 = InputNum("Введите переменную k2: ");
+PointIntersection(b1, k1, b2, k2);
